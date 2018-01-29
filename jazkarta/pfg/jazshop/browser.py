c23f584a6f701eea71e43bfbd36ac1a18edcaaf6
Carlos de la Guardia <cguardia@yahoo.com>
2018-01-29 11:36:07 -0600

    avoid error when order_id is not present in thank you page request

--- jazkarta/pfg/jazshop/browser.py
+++ jazkarta/pfg/jazshop/browser.py
@@ -35,32 +35,34 @@ class JazShopPFGCallback(BrowserView):
             mail_not_sent.replace("_", " ") # decode error message
             self.mail_not_sent = mail_not_sent
 
-        # get cart data from stored order
-        data = get_order_from_id(order_id)
-        self.cart_items = data['items'].items()
+        self.cart_items = []
         self.cart_data = []
         self.amount = 0
         self.ship_method = None
         self.ship_charge = None
         self.taxes = None
-        for order_item in self.cart_items:
-            href = resolve_uid(order_item[1]['uid']).absolute_url()
-            self.cart_data.append({'href': href,
-                                   'quantity': order_item[1]['quantity'],
-                                   'price': order_item[1]['price'],
-                                   'name': order_item[1]['name']})
-            self.amount += order_item[1]['quantity']*order_item[1]['price']
+        if order_id:
+            # get cart data from stored order
+            data = get_order_from_id(order_id)
+            self.cart_items = data['items'].items()
+            for order_item in self.cart_items:
+                href = resolve_uid(order_item[1]['uid']).absolute_url()
+                self.cart_data.append({'href': href,
+                                       'quantity': order_item[1]['quantity'],
+                                       'price': order_item[1]['price'],
+                                       'name': order_item[1]['name']})
+                self.amount += order_item[1]['quantity']*order_item[1]['price']
 
-        if 'ship_charge' in data:
-            self.amount += data['ship_charge']
-            self.ship_charge = data['ship_charge']
+            if 'ship_charge' in data:
+                self.amount += data['ship_charge']
+                self.ship_charge = data['ship_charge']
 
-        if 'taxes' in data:
-            for tax_entry in data['taxes']:
-                self.amount += tax_entry['tax']
-            self.taxes = data['taxes']
+            if 'taxes' in data:
+                for tax_entry in data['taxes']:
+                    self.amount += tax_entry['tax']
+                self.taxes = data['taxes']
 
-        if 'ship_method' in data:
-            self.ship_method = data['ship_method']
+            if 'ship_method' in data:
+                self.ship_method = data['ship_method']
 
         return self.index()
